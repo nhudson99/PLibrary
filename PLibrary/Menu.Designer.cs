7db3a756748d69428c140dbea8feb06e747f3982
Nathan Hudson <nathandhudson@gmail.com>
2019-11-07 13:01:16 -0600

    Added Menu Button Functionality

--- PLibrary/Menu.Designer.cs
+++ PLibrary/Menu.Designer.cs
@@ -31,6 +31,7 @@
             this.btnBookSearch = new System.Windows.Forms.Button();
             this.btnProcessBook = new System.Windows.Forms.Button();
             this.btnTransaction = new System.Windows.Forms.Button();
+            this.btnBookManagement = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // btnBookSearch
@@ -45,12 +46,13 @@
             // 
             // btnProcessBook
             // 
-            this.btnProcessBook.Location = new System.Drawing.Point(418, 265);
+            this.btnProcessBook.Location = new System.Drawing.Point(445, 265);
             this.btnProcessBook.Name = "btnProcessBook";
-            this.btnProcessBook.Size = new System.Drawing.Size(264, 88);
+            this.btnProcessBook.Size = new System.Drawing.Size(197, 88);
             this.btnProcessBook.TabIndex = 1;
             this.btnProcessBook.Text = "Process Books";
             this.btnProcessBook.UseVisualStyleBackColor = true;
+            this.btnProcessBook.Click += new System.EventHandler(this.btnProcessBook_Click);
             // 
             // btnTransaction
             // 
@@ -60,12 +62,24 @@
             this.btnTransaction.TabIndex = 2;
             this.btnTransaction.Text = "Transactions";
             this.btnTransaction.UseVisualStyleBackColor = true;
+            this.btnTransaction.Click += new System.EventHandler(this.btnTransaction_Click);
+            // 
+            // btnBookManagement
+            // 
+            this.btnBookManagement.Location = new System.Drawing.Point(445, 549);
+            this.btnBookManagement.Name = "btnBookManagement";
+            this.btnBookManagement.Size = new System.Drawing.Size(191, 84);
+            this.btnBookManagement.TabIndex = 3;
+            this.btnBookManagement.Text = "Book Management";
+            this.btnBookManagement.UseVisualStyleBackColor = true;
+            this.btnBookManagement.Click += new System.EventHandler(this.btnBookManagement_Click);
             // 
             // Menu
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1108, 691);
+            this.Controls.Add(this.btnBookManagement);
             this.Controls.Add(this.btnTransaction);
             this.Controls.Add(this.btnProcessBook);
             this.Controls.Add(this.btnBookSearch);
@@ -80,6 +94,7 @@
         private System.Windows.Forms.Button btnBookSearch;
         private System.Windows.Forms.Button btnProcessBook;
         private System.Windows.Forms.Button btnTransaction;
+        private System.Windows.Forms.Button btnBookManagement;
     }
 }
 
