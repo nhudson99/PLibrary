3162d75322e3e9eb52e075b3a76ae318075dd7c0
salvador <46605004+salvadoor@users.noreply.github.com>
2019-11-15 17:30:02 -0600

    Added SQL command to insert new transaction

--- PLibrary/CheckOut.cs
+++ PLibrary/CheckOut.cs
@@ -13,8 +13,9 @@ namespace PLibrary
 {
     public partial class CheckOut : Form
     {
-       public SqlConnection DBConnection2;
-       public int books = 0;
+        public SqlConnection DBConnection2;
+        public int books = 0;
+        public int trans_ID = -1;
        // private SqlConnection DBConnection2 = new SqlConnection();
 
         public CheckOut()
@@ -56,9 +57,20 @@ namespace PLibrary
 
         int CreateTransaction()
         {
-            SqlCommand cmdNewTrans = DBConnection2.CreateCommand();
+            DateTime Tdate = DateTime.Now;
+            DateTime Ddate = Tdate.AddDays(14);
 
-            return 1; // change return value after coding command
+            SqlCommand cmdNewTrans = DBConnection2.CreateCommand();
+            cmdNewTrans.CommandText = "INSERT INTO [TRANSACTION] (S_ID, Date, DueDate)";
+            cmdNewTrans.CommandText += "OUTPUT INSERTED.Transaction_ID VALUES (@S_ID, @Date, @DueDate)";
+            cmdNewTrans.Parameters.AddWithValue("@S_ID", SelectAcc.Text);
+            cmdNewTrans.Parameters.AddWithValue("@Date", Tdate);
+            cmdNewTrans.Parameters.AddWithValue("@DueDate", Ddate);
+
+             // t is Transaction_ID
+            int t = (int)cmdNewTrans.ExecuteScalar(); 
+
+            return t;
         }
 
         private void Btn_Start_Click(object sender, EventArgs e)
@@ -77,9 +89,16 @@ namespace PLibrary
         {
             if (SelectAcc.Enabled == false) // check that start button has been pressed
             {
+                books++;
                 if (books == 1)
-                {
-                    // CREATE new TRANSACTION for DB
+                {   // check number of book copies available
+                    if ()
+                    {
+
+                    }
+                    // CREATE new TRANSACTION for DB, get the auto-generated id
+                    trans_ID = CreateTransaction();
+                    MessageBox.Show(trans_ID.ToString());
                 }
 
                 // CREATE new CONATINS_BOOK
